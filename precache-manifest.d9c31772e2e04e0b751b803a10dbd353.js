b180ad3de11be4ee45e30342fa4e1151957b18f7
Julia Taranets <jtaranets@gmail.com>
2019-10-09 17:28:48 +0300

    Updates

---
+++ precache-manifest.d9c31772e2e04e0b751b803a10dbd353.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5b645f1193fc4786c224bcbc6480f0",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "88cefc84f00757cfbdf8",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.c829125e.chunk.css"
+  },
+  {
+    "revision": "6b4245575122fe8eb9d9",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.64375fb5.chunk.css"
+  },
+  {
+    "revision": "88cefc84f00757cfbdf8",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.1dda2b30.chunk.js"
+  },
+  {
+    "revision": "6b4245575122fe8eb9d9",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.5773ce2a.chunk.js"
+  },
+  {
+    "revision": "ec99381c089a361ca4a1",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.6466877b.js"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/goit-react-hw-02-bank-account/static/media/roboto-v20-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/goit-react-hw-02-bank-account/static/media/roboto-v20-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/goit-react-hw-02-bank-account/static/media/roboto-v20-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/goit-react-hw-02-bank-account/static/media/roboto-v20-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/goit-react-hw-02-bank-account/static/media/roboto-v20-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/goit-react-hw-02-bank-account/static/media/roboto-v20-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/goit-react-hw-02-bank-account/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/goit-react-hw-02-bank-account/static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  }
+]);
